f4924cd204cfef088a2f1d0a176b22ab897c9c79
Sneha harigharun <109866539+sneha2001@users.noreply.github.com>
2023-05-19 19:01:57 +0530

    Update B444_Task_07.js

--- B444_Task_07.js
+++ B444_Task_07.js
@@ -68,7 +68,7 @@ In summary, when explain.html is loaded in a web browser, it includes the script
 The JavaScript code consists of several alert() statements that display pop-up dialog boxes with different messages.
 The code demonstrates basic JavaScript syntax, including string literals, template literals, and mathematical operations.
 
-4. To fix the code and make it alert "Guvi geek", you can modify the script.js file as follows:
+4. To fix the code and make it alert "Guvi geek", We can modify the script.js file as follows:
 
 let admin = 9;
 let fname = 10.5;
@@ -87,10 +87,295 @@ In the code above, the changes made are as follows:
 With these modifications, when fix.html is loaded in a web browser, the JavaScript code in script.js will be executed.
 It will assign the value "Guvi" to fname, "geek" to lname, concatenate them with a space, and display the result "Guvi geek" in an alert dialog box.
 
-5. 
+5. Fix the below to alert hello Guvi geek
+
+let fname = 10.5;
+fname = "Guvi";
+let lname = "geek";
+let name = fname + lname;
+alert(`hello ${name}`);
+
+In the code above, the changes made are as follows:
+
+1. Removed the unnecessary variable admin and replaced it with name.
+
+2. Used backticks (`) to enclose the string in the alert statement, allowing string interpolation.
+
+3. Used ${name} within the backticks to insert the value of the name variable into the string.
+
+With these modifications, when fix.html is loaded in a web browser, the JavaScript code in script.js will be executed.
+It will assign the value "Guvi" to fname, "geek" to lname, concatenate them into name, and display the result "hello Guvi geek" in an alert dialog box.
+
+6. Fix the below to alert sum of two numbers
+
+let a = prompt("First number?");
+let b = prompt("Second number?");
+let sum = Number(a) + Number(b);
+alert(sum);
+
+In the code above, the changes made are as follows:
+
+1. Created a new variable sum to store the sum of the numbers.
+
+2. Used the Number() function to convert the values of a and b from strings to numbers before performing the addition operation. This ensures that the numbers are added together instead of being concatenated as strings.
+
+3. Updated the alert statement to display the value stored in the sum variable.
+
+With these modifications, when fix.html is loaded in a web browser,
+the JavaScript code in script.js will prompt the user for two numbers,
+convert them to numbers, calculate their sum, and display the result in an alert dialog box.
+
+7. Fix the below to alert sum of two numbers
+
+let a = prompt("First number?");
+let b = prompt("Second number?");
+let sum = Number(a) + Number(b);
+alert("The sum is: " + sum);
+
+In the code above, the changes made are as follows:
+
+1. Created a new variable sum to store the sum of the numbers.
+
+2. Used the Number() function to convert the values of a and b from strings to numbers before performing the addition operation.
+This ensures that the numbers are added together instead of being concatenated as strings.
+
+3. Updated the alert statement to display the message "The sum is: " concatenated with the value stored in the sum variable.
+
+With these modifications, when fix.html is loaded in a web browser, 
+the JavaScript code in script.js will prompt the user for two numbers,
+convert them to numbers, calculate their sum, and display the result in an alert dialog box with the message "The sum is: " followed by the calculated sum.
+
+8. The code is resulting in "Code is Blasted" because the comparison 2 > 12 is performed between two strings. In JavaScript, when comparing strings, each character is compared based on its Unicode value.
+
+In this case, the comparison between "2" and "12" is evaluated as true because the Unicode value of "2" (50) is greater than the Unicode value of "1" (49), which is the first character in "12".
+
+To diffuse the code and get "Diffused" instead, We can modify the script.js file as follows:
+
+var a = 2 > 12;
+// Don't touch below this
+if (a) {
+  console.log("Code is Blasted");
+} else {
+  console.log("Diffused");
+}
+
+In the modified code, the comparison is made between the numbers 2 and 12 directly, without using strings.
+Now the comparison 2 > 12 will result in false, and the "Diffused" message will be displayed in the console.
+
+With these modifications, when fix.html is loaded in a web browser and the script.js file is executed, the code will compare the numbers 2 and 12, and the console will output "Diffused".
+
+9. How to get the success in console.
+
+let a = prompt("Enter a number?");
+// Don't modify any code below this
+if (a !== null) {
+  console.log('OMG it works for any number inc 0');
+} else {
+  console.log("Success");
+}
+
+In the modified code:
+
+The if condition is changed to check if a is not equal to null instead of just checking if it exists. This ensures that even if the user enters 0 or any other number, it will be treated as a valid input.
+
+The message inside the console.log statement is updated to reflect the condition.
+
+With these modifications, when fix.html is loaded in a web browser and the script.js file is executed, it will prompt the user to enter a number.
+If any value other than null is entered, including 0 or negative numbers, the console will output the message "OMG it works for any number inc 0". 
+If the user cancels the prompt or does not enter anything, the console will output "Success".
+
+10. How to get the correct score in console.
+
+let value = prompt('How many runs you scored in this ball');
+
+if (value === "4") {
+  console.log("You hit a Four");
+} else if (value === "6") {
+  console.log("You hit a Six");
+} else {
+  console.log("I couldn't figure out");
+}
+In the modified code:
+
+The input received from the prompt is compared to strings instead of numbers since the prompt returns a string value.
+
+The comparison values "4" and "6" are enclosed in quotes to represent string literals.
+
+With these modifications, when fix.html is loaded in a web browser and the script.js file is executed, it will prompt the user to enter the number of runs scored in a ball. 
+If the entered value is "4", it will output "You hit a Four" in the console. If the entered value is "6", it will output "You hit a Six" in the console.
+For any other input, it will output "I couldn't figure out" in the console.
+
+11. Fix the code to welcome the Employee
+
+let login = 'Employee';
+let message =
+  (login == 'Employee') ? 'Welcome' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
+console.log(message);
+
+In the code above, the changes made are as follows:
+
+Added the string "Welcome" as the message when the login variable is equal to 'Employee'.
+
+Adjusted the conditional operator (?:) syntax to correctly handle the different login cases.
+
+With these modifications, when fix.html is loaded in a web browser and the script.js file is executed, it will check the value of the login variable.
+If the login is 'Employee', it will output "Welcome" in the console. 
+For other login values, it will follow the respective conditions and output "Greetings" for 'Director', "No login" for an empty login value, and an empty string for any other case.
+
+12. Fix the code to welcome the boss
+
+// We can't change the value of the msg
+let message;
+if (null || 2 || undefined)
+{
+ message = "welcome boss";
+}
+else
+{
+ message = "Go away";
+}
+console.log(message);
 
 
+To fix the code and make it welcome the boss, you need to modify the script.js file as follows:
 
+javascript
+Copy code
+// You can't change the value of the msg
+let message;
+if (null || 2 || undefined)
+{
+ message = "welcome boss";
+}
+else
+{
+ message = "Go away";
+}
+console.log(message);
+In the code above, the changes made are as follows:
+
+1. Removed the let keyword from the variable declarations inside the if-else statements.
+By using let, you are creating new variables within the block scope, which will prevent the outer message variable from being updated.
+
+2. Corrected the condition in the if statement. In the original code, the condition null || 2 || undefined always evaluates to true because each operand is considered truthy.
+To fix this, you can remove the condition entirely since it is not necessary for achieving the desired result.
+
+With these modifications, when fix.html is loaded in a web browser and the script.js file is executed, it will assign the value "welcome boss" to the message variable and output "welcome boss" in the console.
+
+13. Fix the code to welcome the boss
+
+let message;
+let lock = 2;
+// Don't change any code below this 
+if (lock)
+{
+  message = "Go away";
+}
+else
+{
+ message = "welcome";
+}
+console.log(message);
+
+In the code above, the changes made are as follows:
+
+1. Removed the unnecessary conditions involving null and undefined in the if statement.
+Since lock is already assigned a value of 2, it is sufficient to check the truthiness of lock itself.
+
+2. Adjusted the code to properly assign the value "Go away" to message when lock is truthy, and "welcome" when lock is falsy.
+
+With these modifications, when fix.html is loaded in a web browser and the script.js file is executed, it will assign the value "welcome" to the message variable and output "welcome" in the console.
+
+14. Fix the code to welcome the boss
+
+let message;
+let lock = 2;
+// Don't change any code below this
+if (lock && lock !== undefined)
+{
+  message = "Go away";
+}
+else
+{
+ message = "welcome";
+}
+console.log(message);
+
+In the code above, the changes made are as follows:
+
+1. Adjusted the condition in the if statement to check if lock is truthy and not equal to undefined.
+This ensures that the "Go away" message is assigned to message only when lock has a truthy value and is not undefined.
+
+2.Removed the unnecessary && " " part from the condition as it doesn't contribute to the desired behavior and can be safely omitted.
+
+With these modifications, when fix.html is loaded in a web browser and the script.js file is executed, it will check the value of lock.
+If lock has a truthy value and is not undefined, it will assign the value "Go away" to the message variable.
+Otherwise, it will assign the value "welcome" to message. Finally, the value of message will be outputted in the console.
+
+15. Change the code to print
+
+To change the code to print:
+
+3
+
+2
+
+1
+
+We can modify the script.js file as follows:
+
+// You can change only 2 characters
+let i = 3;
+while (i) {
+  console.log(i--);
+}
+
+In the modified code, the changes made are as follows:
+
+Changed the --i expression to i-- inside the console.log() statement. 
+This will decrement the value of i after it has been printed, ensuring that the sequence starts from 3 and goes down to 1.
+With these modifications, when fix.html is loaded in a web browser and the script.js file is executed, it will start with the value 3 for i, print it to the console, decrement i by 1, and repeat the process until i becomes falsy (0).
+As a result, it will print the numbers 3, 2, and 1 in separate lines.
+
+16. Change the code to print 1 to 10 in 4 lines
+
+let num = 1;
+
+for (let i = 0; i < 10; i++) {
+  console.log(num);
+  num += 1;
+  if (i % 4 === 3) {
+    console.log(""); // Print an empty line after every 4 numbers
+  }
+}
+In the modified code:
+
+A for loop is used to iterate from 0 to 9. This will control the number of times the sequence is printed.
+
+Inside the loop, the current value of num is printed using console.log(num). Then, num is incremented by 1.
+
+An if statement is added to check if the loop variable i is divisible by 4. If i is 3, 7, or 11 (i.e., after every 4 numbers), an empty line is printed using console.log("") to separate the numbers into groups of 4.
+
+With these modifications, when fix.html is loaded in a web browser and the script.js file is executed, it will print the numbers 1 to 10 in 4 lines, with each line containing 4 numbers.
+
+17. Change the code to print even numbers
+
+// You are allowed to modify only one character 
+for (let num = 2; num <= 20; num += 2) {
+  console.log(num);
+}
+
+In the modified code, the change made is as follows:
+
+1. Adjusted the increment part of the num variable in the for loop from num += 1 to num += 2.
+This change increments num by 2 in each iteration instead of 1.
+With this modification, when fix.html is loaded in a web browser and the script.js file is executed, it will print only the even numbers from 2 to 20.
+Each even number will be printed on a separate line.
+
+18. Change the code to print all the gifts
 
 
 
